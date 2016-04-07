1f1e11198f33afa8f3d7e29f0daf012428a4cf47
Baochuan Lu <lubaochuan@gmail.com>
2016-04-07 14:25:01 +0000

    Make Mailbox thread-safe

--- Mailbox.java
+++ Mailbox.java
@@ -1,10 +1,33 @@
 /**
  * one producer and one consumer communicate via a shared buffer
+ * 
  * @author Baochuan Lu
  */
 public class Mailbox{
   public boolean request;
   public String message;
+  
+  public synchronized void storeMessage(String newMessage){
+    while (request == true){ // has a message 
+      try{
+        wait();
+      } catch(InterruptedException e){}
+    }
+    request = true;
+    this.message = newMessage;
+    notifyAll();
+  }
+  
+  public synchronized String retrieveMessage(){
+    while (request == false){ // empty box
+      try{
+        wait();
+      } catch(InterruptedException e){}
+    }
+    request = false;
+    notifyAll();
+    return message;
+  }
 
   public static void main(String[] args){
     Mailbox mb = new Mailbox();
@@ -25,14 +48,8 @@ class Producer extends Thread{
   public void run(){
     int index = 0;
     while (true){
-      if (!myMailbox.request){
-        myMailbox.message = "Hello-"+(index++);
-        //System.err.println("send   :"+myMailbox.message);
-        myMailbox.request = true;
-      }
-      try{
-        sleep(50);
-      }catch(InterruptedException e){}
+      String message = "Hello-"+(index++);
+      myMailbox.storeMessage(message);
     }
   }
 }
@@ -46,24 +63,8 @@ class Consumer extends Thread{
   
   public void run(){
     while (true){
-      if (myMailbox.request){
-        System.err.println("received:"+myMailbox.message);
-        myMailbox.request = false;
-      }
-      try{
-        sleep(50);
-      }catch(InterruptedException e){}
+      String m = myMailbox.retrieveMessage();
+      System.err.println("received:"+m);
     }
   }
 }
-
-/*
-Issues:
-1. The consumer class accesses data internal to the Mailbox class, introducing 
-   the possibility of corruption. The consumer thread could be interrupted 
-   after if(myMailbox.request). The interrupting thread could be a client that 
-   sets message to its own message ignoring the convention of checking request 
-   to see if the handler is available.
-2. The choice of 50 ms for the delay can never by ideal.The thread should back 
-   off for a while, but for how long? There is no good answer to this question.
-*/
